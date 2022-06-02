0e629f383071946c20b2f0f586f8dd6fe8c5ce53
Dylan Finney <dylan.finney@outlook.com>
2022-06-02 11:50:54 +0100

    Ancestry

---
+++ packages/ancestry-data/__tests__/ancestry.test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const MOCK = require("@dynamic-data/ancestry-data");
+
+const { RawDataObject } = require("@dynamic-data/ancestry-data");
+
+describe("Ancestry", () => {
+  //test.todo("needs tests");
+  it("Mockup datamodels from default", () => {
+    console.log("MOCK ", MOCK);
+    expect(typeof MOCK).toBe("object");
+  });
+  it("Expanded RawDataObject datamodel from default", () => {
+    console.log("MOCK ", JSON.stringify(RawDataObject, undefined, 2));
+    expect(typeof MOCK).toBe("object");
+  });
+});
