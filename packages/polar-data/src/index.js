c58efbfad00f7034712402437df981b46049f71a
Jashaul <djashaul@gmail.com>
2022-08-02 10:52:41 -0400

    polar dynamic data init

---
+++ packages/polar-data/src/index.js
@@ -0,0 +1,50 @@
+import {
+  getUserInfo,
+  listNotifications,
+  listActivities,
+  getActivitySummary,
+  getStepSample,
+  getZoneSample,
+  listTrainingExercises,
+  getExerciseSummary,
+  getHeartRateZones,
+  getAvailableSamples,
+  getSamples,
+  listPhysicalInfo,
+  getPhysicalInfo,
+  getWebhook,
+  listExercises,
+  getExercise,
+  listSleep,
+  getSleep,
+  getAvailableSleepTimes,
+  listNightlyRecharge,
+  getNightlyRecharge
+} from "./models";
+  
+const Data = {
+  getUserInfo,
+  listNotifications,
+  listActivities,
+  getActivitySummary,
+  getStepSample,
+  getZoneSample,
+  listTrainingExercises,
+  getExerciseSummary,
+  getHeartRateZones,
+  getAvailableSamples,
+  getSamples,
+  listPhysicalInfo,
+  getPhysicalInfo,
+  getWebhook,
+  listExercises,
+  getExercise,
+  listSleep,
+  getSleep,
+  getAvailableSleepTimes,
+  listNightlyRecharge,
+  getNightlyRecharge
+};
+
+export default Data;
+  
