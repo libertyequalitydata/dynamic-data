43c9d647a01457a86e8a46008cfcb64a9e28420e
Dylan Finney <dylan.finney@outlook.com>
2022-11-21 15:04:34 +0000

    utils additions

--- packages/utils/src/randomUtils.js
+++ packages/utils/src/randomUtils.js
@@ -739,3 +739,34 @@ export function getRandomTimeZone() {
 export function getAverage(array) {
   return array.reduce((a, b) => a + b) / array.length;
 } 
+
+export function calculateSum (array) {
+  return array.reduce((total, current) => {
+      return total + current;
+  }, 0);
+}
+
+export function linearRegression(y,x){
+  var lr = {};
+  var n = y.length;
+  var sum_x = 0;
+  var sum_y = 0;
+  var sum_xy = 0;
+  var sum_xx = 0;
+  var sum_yy = 0;
+
+  for (var i = 0; i < y.length; i++) {
+
+      sum_x += x[i];
+      sum_y += y[i];
+      sum_xy += (x[i]*y[i]);
+      sum_xx += (x[i]*x[i]);
+      sum_yy += (y[i]*y[i]);
+  } 
+
+  lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
+  lr['intercept'] = (sum_y - lr.slope * sum_x)/n;
+  lr['r2'] = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
+
+  return lr;
+}
