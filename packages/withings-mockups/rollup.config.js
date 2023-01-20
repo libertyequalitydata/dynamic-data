ed88b035f4e44d962f9fe42bea918a86d5ee272d
Dylan Finney <dylan.finney@outlook.com>
2023-01-20 17:04:00 +0000

    Rollup Config Resolve

--- packages/withings-mockups/rollup.config.js
+++ packages/withings-mockups/rollup.config.js
@@ -19,6 +19,7 @@ export default [
       },
     ],
     plugins: [
+      externals(),
       resolve({ extensions }),
       babel({
         babelHelpers: "bundled",
@@ -26,7 +27,7 @@ export default [
         extensions,
         exclude: "./node_modules/**",
       }),
-      exclude()
+      
     ],
   },
 ];
