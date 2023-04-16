1884605a414ccfba39c6a458c874564564bd2c5f
prifina-admin <tero@prifina.com>
2023-04-16 15:55:00 +0300

    rollup external fix

--- packages/fitbit-mockups/rollup.config.js
+++ packages/fitbit-mockups/rollup.config.js
@@ -1,11 +1,29 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
-import externals from 'rollup-plugin-node-externals'
+//import externals from 'rollup-plugin-node-externals'
 
 const extensions = [".js"];
 
+const pkg = require('./package.json');
+
+const externalPackages = [
+  //...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+// Creating regexes of the packages to make sure subpaths of the
+// packages are also treated as external
+const regexesOfPackages = externalPackages
+  .map(packageName => new RegExp(`^${packageName}(\/.*)?`));
+/*
+export default {
+  external: regexesOfPackages,
+  // ...
+};
+*/
+
 export default [
   {
+    external: regexesOfPackages,
     input: "src/index.js",
     output: [
       {
@@ -19,7 +37,7 @@ export default [
       },
     ],
     plugins: [
-      externals(),
+      //externals(),
       resolve({ extensions }),
       babel({
         babelHelpers: "bundled",
@@ -27,7 +45,7 @@ export default [
         extensions,
         exclude: "./node_modules/**",
       }),
-      
+
     ],
   },
 ];
